fb2a59c6057af2e68a549ba7d04a006d89a3c04f
NICOLAS ALBUQUERQUE RAMOS <2021325683@IFAL-MACEIO.INTRANET>
2023-03-29 10:20:58 -0300

    :rocket: added 02.sql

---
+++ SELECT/02.sql
@@ -0,0 +1,4 @@
+-- Selecione uma lista que contenha número da matrícula, nome, sobrenome e cargo de todos os funcionários do sexo feminino.
+
+SELECT CD_MAT, NM_FUNC, NM_SOBRENOME, NR_CARGO
+FROM FUNC
