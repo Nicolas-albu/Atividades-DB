cedebb41a8257bd49d49798a6595d931f3ce116b
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-03-29 10:23:45 -0300

    :rocket: added 03.sql

---
+++ SELECT/03.sql
@@ -0,0 +1,5 @@
+-- Selecione todos os cargos ocupados pelos funcionários. Exiba apenas uma
+-- ocorrência para cada cargo. Exiba a coluna com o nome COD_CARGO.
+
+SELECT DISTINCT NR_CARGO AS COD_CARGO
+FROM FUNC
