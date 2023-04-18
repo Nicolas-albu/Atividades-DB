ddd98e4173edbe6411d662cc7e8d8ba2253abc27
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-04-18 20:50:44 -0300

    Adição do exercício 07

--- FUNCOES/07.sql
+++ FUNCOES/07.sql
@@ -1,8 +1,21 @@
-
 -- Deseja-se uma relação contendo nome, sobrenome e o salário (editado com
 -- moeda) para os funcionários com mais de 4 vogais em seu nome.
-
 SELECT FUNC.NM_FUNC AS NOME,
-       FUNC.NM_SOBRENOME AS SOBRENOME,
-       TO_CHAR(FUNC.VL_SAL, 'L999G990D00') AS SALÁRIO
+    FUNC.NM_SOBRENOME AS SOBRENOME,
+    TO_CHAR(FUNC.VL_SAL, 'L999G990D00') AS SALÁRIO
 FROM FUNC
+WHERE LENGTH(FUNC.NM_FUNC || FUNC.NM_SOBRENOME) - LENGTH(
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    REPLACE(
+                        REPLACE(FUNC.NM_FUNC || FUNC.NM_SOBRENOME, 'A'),
+                        'E'
+                    ),
+                    'I'
+                ),
+                'O'
+            ),
+            'U'
+        )
+    ) > 4
