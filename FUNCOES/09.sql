532f375c3de8dc4308051376283ad5eb9000be6a
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-04-18 21:26:55 -0300

    Atualização da atividade 09

--- FUNCOES/09.sql
+++ FUNCOES/09.sql
@@ -2,12 +2,13 @@
 -- primeira letra do nome em maiúscula e as demais em minúscula, sexo por extenso,
 -- número de dias entre a data de admissão e o último dia do mês da data corrente.
 
-        --  CASE WHEN F.CD_SEXO = 'M' THEN 'Masculino'
-        --         ELSE 'Feminino'
-        --  END AS "SEXO",
-        --  LAST_DAY(CURRENT_DATE) - F.DT_ADMISSAO AS "DIAS DE EMPREGADO"
+--  CASE WHEN F.CD_SEXO = 'M' THEN 'Masculino'
+--         ELSE 'Feminino'
+--  END AS "SEXO",
+--  LAST_DAY(CURRENT_DATE) - F.DT_ADMISSAO AS "DIAS DE EMPREGADO"
 
 SELECT INITCAP(F.NM_FUNC || ' ' || F.NM_SOBRENOME) AS "NOME COMPLETO",
-       REPLACE(REPLACE(F.IN_SEXO, 'M', 'MASCULINO'), 'F', 'FEMININO') AS SEXO,
-       TRUNC(LAST_DAY(SYSDATE) - F.DT_ADM) AS "DIAS DE EMPREGADO"
+    --    REPLACE(REPLACE(F.IN_SEXO, 'M', 'MASCULINO'), 'F', 'FEMININO') AS SEXO,
+       DECODE(F.IN_SEXO, 'F', 'FEMININO', 'M', 'MASCULINO'),
+       TRUNC(LAST_DAY(SYSDATE) - F.DT_ADM) AS "DIAS DE EMPREGADO" AS SEXO
 FROM FUNC F
