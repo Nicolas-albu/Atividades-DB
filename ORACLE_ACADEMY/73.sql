91504c3f9386982129aa5af9c30729554d29a0c6
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-04-27 23:50:20 -0300

    Adição de resolução 73 da Oracle Academy

---
+++ ORACLE_ACADEMY/73.sql
@@ -0,0 +1,20 @@
+-- A Global Fast Foods está expandindo sua equipe. A gerente, Monique Tuttle, contratou Kai Kim.
+-- Nem todas as informações estão disponíveis no momento, mas adicione as exibidas à direita.
+-- ID FIRST_NAME LAST_NAME BIRTHDATE   SALARY   STAFF_TYPE
+-- 25   Kai         Kim    3-Nov-1988   6.75   Order Taker
+
+INSERT INTO COPY_F_STAFFS (
+    ID,
+    FIRST_NAME,
+    LAST_NAME,
+    BIRTHDATE,
+    SALARY,
+    STAFF_TYPE
+) VALUES (
+    25,
+    'Kai',
+    'Kim',
+    '3-Nov-1988',
+    6.75,
+    'Order Taker'
+);
